fd3a4e0ba9a7a1e2789a533ef02785b703c000af
alexey <alexey@master.hadoop.vm>
2014-02-21 14:23:42 +0100

    added argument parsing

--- mapper.py
+++ mapper.py
@@ -4,12 +4,12 @@ import sys,os
 import argparse
 import subprocess
 
-parser = argparse.ArgumentParser(description='Wrapper for the HTSeq-count utility')
-parser.add_argument('-s','--sam', help='SAM file, for Hadoop it should be a STDIN, i.e. a "-"',default="-", required=True)
-parser.add_argument('-g','--gff', help='GFF file', required=True)
+parser = argparse.ArgumentParser(description="""Wrapper for the HTSeq-count utility. \
+alexey.siretskiy@it.uu.se, 2014""")
+parser.add_argument('sam', help='SAM file, for Hadoop it should be a STDIN, i.e. a "-"',default="-")
+parser.add_argument('gff', help='the folder containing the GFF file')
 parser.add_argument('-m','--mode',help='''mode to handle reads overlapping more than one
-                        feature(choices: union, intersection-strict,
-                        intersection-nonempty; default: union)''', default='union')
+                        feature (default is  'union')''', default='union', choices=['union', 'intersection-strict', 'intersection-nonempty'])
 
 parser.add_argument('-t', '--type',help='''
                         feature type (3rd column in GFF file) to be used, all
@@ -20,17 +20,25 @@ parser.add_argument('-i', '--idattr', help='''
                         GFF attribute to be used as feature ID (default,
                         suitable for Ensembl GTF files: gene_id)''',default='gene_id')
 
+parser.add_argument('-q','--quiet', action='store_true', help="suppress progress report and warnings")
 
 args = vars(parser.parse_args())
+#print args
+
 
 import glob
-gffFile = glob.glob(args['gff']+"/*.gff")[0]
-
-
-#print args
+try:
+	gffFile = glob.glob(args['gff']+"/*.gff")[0]
+except:
+	print "you should point to the DIRECTORY containing the GFF file"
+	raise
+	
 from subprocess import call
-cmd = ["htseq-count", "--type="+args['type'], "--idattr="+args['idattr'], args['sam'], gffFile] 
-print cmd
+cmd = ["htseq-count", "--type="+args['type'], "--idattr="+args['idattr'], "--mode="+args['mode']]
+if args['quiet']: cmd.append("--quiet")
+cmd+=[args['sam'], gffFile] 
+
+#print cmd
 call(cmd)
 
 #call(["readlink","-f", args['gff']])
